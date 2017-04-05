a527993f248d8f45efbb9521073c405953ff10b1
cbogues <YOUR-EMAIL-ADDRESS>
2017-04-04 20:39:52 -0700

    added credentials and built top terms controller

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,13 @@
+var core = require('./controllers/core');
+
 module.exports = function(app) {
-	app.get('/', function (req, res) {
-		res.send('this is the homepage');
-	})
-	app.get('/top', function(req, res) {
-		res.send('this is the top page');
-	})
+	app.get('/', core.home);
+
+	// app.get('/top', function(req, res) {
+	// 	res.send('this is the top page');
+	// })
+	app.get('/top', core.top);
+	
 	app.get('/results', function(req, res) {
 		res.send('this is the results page');
 	})
