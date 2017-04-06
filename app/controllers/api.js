19533c71ae7fdc03274b0f29de4a0d4ddcdcd67f
cbogues <YOUR-EMAIL-ADDRESS>
2017-04-05 20:20:17 -0700

    finished the UI and bootstrap

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -3,12 +3,13 @@ var client = require('../util/twitter');
 
 exports.results = function(req, res){
 	var query = req.query.q;
-	if(query) {
+	if(query){
 		db.getCollection('searches').insert({term: query});
 		db.saveDatabase();
 
 		client.get('search/tweets', {q:query}, function(error, tweets, response){
 			res.json(tweets);
+		})
 	} else {
 		res.send('Error');
 	}
